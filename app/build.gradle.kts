5498b0be5b759e7f182f8e32353dbb974534260d
Emma <emma.tellblom@gmail.com>
2024-07-25 20:14:51 +0200

    New release due to fix of import/export

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mensinator.app"
         minSdk = 30
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.7"
+        versionCode = 7
+        versionName = "1.71"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
