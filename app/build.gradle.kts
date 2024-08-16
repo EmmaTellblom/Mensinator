0670d7c04e37b7a90ef9c69edd4ca11c67242772
Emma <emma.tellblom@gmail.com>
2024-08-16 19:36:10 +0200

    New release 1.8.2 for F-Droid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mensinator.app"
         minSdk = 30
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.8.1"
+        versionCode = 11
+        versionName = "1.8.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
