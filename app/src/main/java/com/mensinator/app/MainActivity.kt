89697e8a575258cdf7c99287703097538a4e7e51
rubens23 <rubens_assis@outlook.com.br>
2024-08-19 15:17:14 -0300

    implemented screen protection on the settings of the app

--- app/src/main/java/com/mensinator/app/MainActivity.kt
+++ app/src/main/java/com/mensinator/app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mensinator.app
 import android.os.Bundle
+import android.util.Log
 import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -10,13 +11,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        window?.setFlags(
-            WindowManager.LayoutParams.FLAG_SECURE,
-            WindowManager.LayoutParams.FLAG_SECURE
-        )
         setContent {
             MensinatorTheme {
-                CalendarScreen()  // Call the CalendarScreen composable from the ui package
+                // Call the CalendarScreen composable from the ui package
+                CalendarScreen{
+                    isScreenProtectionEnabled->
+                    // Sets the flags for screen protection if
+                    // isScreenProtectionEnabled == true
+                    // If isScreenProtectionEnabled == false it removes the flags
+                    if(isScreenProtectionEnabled){
+                        window?.setFlags(
+                            WindowManager.LayoutParams.FLAG_SECURE,
+                            WindowManager.LayoutParams.FLAG_SECURE
+                        )
+                    }else{
+                        window?.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+                    }
+
+                }
             }
         }
     }
