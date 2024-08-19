3167e18ebc1254a42ced9a6eae0d7a85ef1242b1
rubens23 <rubens_assis@outlook.com.br>
2024-08-19 12:05:49 -0300

    Implemented screen protection

--- app/src/main/java/com/mensinator/app/MainActivity.kt
+++ app/src/main/java/com/mensinator/app/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.mensinator.app
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        window?.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
         setContent {
             MensinatorTheme {
                 CalendarScreen()  // Call the CalendarScreen composable from the ui package
