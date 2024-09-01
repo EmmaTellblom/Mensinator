11dff84779a183dcd8c6bf81ef416c089b9a1a1c
dimitrisKlokidis <dimitrisklokidis@gmail.com>
2024-09-01 21:39:48 +0300

    centered the date of the month on the top

--- app/src/main/java/com/mensinator/app/StatisticsDialog.kt
+++ app/src/main/java/com/mensinator/app/StatisticsDialog.kt
@@ -1,23 +1,17 @@
 package com.mensinator.app
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Card
-import androidx.compose.material3.Divider
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
