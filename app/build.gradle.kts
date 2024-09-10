f0da9fca8af931d86ed276c83268310729f4f6a9
Emma <emma.tellblom@gmail.com>
2024-09-10 21:23:52 +0200

    Increased version for testing purpose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.mensinator.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 14
-        versionName = "1.8.5"
+        versionCode = 15
+        versionName = "1.8.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
