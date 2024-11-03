88a5fb966e544c59b94555196ffa263b27593916
Carsten Hagemann <carsten@chagemann.de>
2024-11-03 20:56:50 +0100

    Remove unused imports/variables

--- app/src/main/java/com/mensinator/app/ManageSymptomScreen.kt
+++ app/src/main/java/com/mensinator/app/ManageSymptomScreen.kt
@@ -25,7 +25,6 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Switch
-import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
