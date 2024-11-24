8fde17c148e2e28469c736c63fcfdb6ed37fcfdf
Emma <emma.tellblom@gmail.com>
2024-11-24 10:37:44 +0100

    Small release (1.9.3) to fix issue with FDroid and IzzyOnDroid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.mensinator.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 22
-        versionName = "1.9.2"
+        versionCode = 23
+        versionName = "1.9.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
