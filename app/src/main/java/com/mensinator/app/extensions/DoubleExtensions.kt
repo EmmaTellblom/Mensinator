01ca4f93bb84a164232fc384cbdff721d6a4101a
Carsten Hagemann <carsten@chagemann.de>
2024-11-30 13:56:45 +0100

    Make sure it's fine on all font sizes

--- app/src/main/java/com/mensinator/app/extensions/DoubleExtensions.kt
+++ app/src/main/java/com/mensinator/app/extensions/DoubleExtensions.kt
@@ -4,6 +4,7 @@ import java.util.Locale
 import kotlin.math.round
 
 fun Double.formatToOneDecimalPoint(): String {
+    if (this.isNaN()) return "-"
     return String.format(Locale.getDefault(), "%.1f", this)
 }
 
--- app/src/main/java/com/mensinator/app/statistics/StatisticsScreen.kt
+++ app/src/main/java/com/mensinator/app/statistics/StatisticsScreen.kt
@@ -10,8 +10,10 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mensinator.app.R
 import com.mensinator.app.navigation.displayCutoutExcludingStatusBarsPadding
@@ -42,43 +44,36 @@ private fun StatisticsScreenContent(
             .fillMaxSize()
             .verticalScroll(rememberScrollState())
             .displayCutoutExcludingStatusBarsPadding()
-            .padding(16.dp)
+            .padding(horizontal = 16.dp)
     ) {
         RowOfText(
             stringResource(id = R.string.period_count),
             state.trackedPeriods
         )
-
         RowOfText(
             stringResource(id = R.string.average_cycle_length),
             state.averageCycleLength
         )
-
         RowOfText(
             stringResource(id = R.string.average_period_length),
             state.averagePeriodLength
         )
-
         RowOfText(
             stringResource(id = R.string.next_period_start_future),
             state.periodPredictionDate
         )
-
         RowOfText(
             stringResource(id = R.string.ovulation_count),
             state.ovulationCount
         )
-
         RowOfText(
             stringResource(id = R.string.average_ovulation_day),
             state.follicleGrowthDays
         )
-
         RowOfText(
             stringResource(id = R.string.next_predicted_ovulation),
             state.ovulationPredictionDate
         )
-
         RowOfText(
             stringResource(id = R.string.average_luteal_length),
             state.averageLutealLength
@@ -93,18 +88,21 @@ fun RowOfText(stringOne: String, stringTwo: String?) {
             .fillMaxWidth()
             .padding(vertical = 16.dp),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Start
+        horizontalArrangement = Arrangement.Absolute.SpaceAround
     ) {
         Text(
             text = stringOne,
+            modifier = Modifier
+                .weight(0.7f)
+                .padding(end = 8.dp),
             style = MaterialTheme.typography.bodyLarge
         )
-        Spacer(modifier = Modifier.weight(1f))
         stringTwo?.let {
             Text(
                 text = it,
-                modifier = Modifier.alignByBaseline(),
-                style = MaterialTheme.typography.titleMedium
+                modifier = Modifier.alignByBaseline().widthIn(max = 200.dp),
+                style = MaterialTheme.typography.titleMedium,
+                textAlign = TextAlign.End
             )
         }
     }
@@ -114,6 +112,18 @@ fun RowOfText(stringOne: String, stringTwo: String?) {
 @Composable
 private fun RowOfTextPreview() {
     RowOfText("firstString", "secondstring")
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RowOfTextLongPreview() {
+    RowOfText("Very long first string, we could even use lorem ipsum here", "secondstring")
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun RowOfTextLongSecondPreview() {
+    RowOfText("Short Text", "first string, we could even use lorem ipsum here")
 }
 
 @Preview(showBackground = true)
