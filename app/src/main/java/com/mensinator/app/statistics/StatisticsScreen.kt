31e1b7fcf3d316d189a898f553a735f199f16667
Carsten Hagemann <carsten@chagemann.de>
2024-11-30 14:05:11 +0100

    Remove custom typography that is not defined properly anyhow

--- app/src/main/java/com/mensinator/app/statistics/StatisticsScreen.kt
+++ app/src/main/java/com/mensinator/app/statistics/StatisticsScreen.kt
@@ -13,7 +13,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.max
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mensinator.app.R
 import com.mensinator.app.navigation.displayCutoutExcludingStatusBarsPadding
