63a77013449415c2e7046516f61d3eedf9a41af8
Carsten Hagemann <carsten@chagemann.de>
2025-04-07 21:31:29 +0200

    Rebase, make widget itself work, add todos

--- app/src/main/java/com/mensinator/app/MensinatorWidget.kt
+++ app/src/main/java/com/mensinator/app/MensinatorWidget.kt
@@ -2,69 +2,121 @@ package com.mensinator.app
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.glance.GlanceId
-import androidx.glance.GlanceModifier
-import androidx.glance.GlanceTheme
-import androidx.glance.appwidget.GlanceAppWidget
-import androidx.glance.appwidget.GlanceAppWidgetReceiver
-import androidx.glance.appwidget.updateAll
-import androidx.glance.layout.*
-import androidx.glance.text.Text
+import androidx.glance.*
 import androidx.glance.action.actionStartActivity
 import androidx.glance.action.clickable
-import androidx.glance.appwidget.provideContent
-import androidx.glance.background
+import androidx.glance.appwidget.*
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.padding
+import androidx.glance.text.Text
+import androidx.glance.text.TextStyle
+import com.mensinator.app.business.IPeriodPrediction
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import androidx.glance.layout.Column
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
+import org.koin.core.context.GlobalContext
+import java.time.LocalDate
 
-class MensinatorWidget : GlanceAppWidget() {
+sealed interface NextPeriodFormat {
+    data object OnlyDays : NextPeriodFormat
+    data object MediumLengthText : NextPeriodFormat
+}
+
+// TODO: Check what widgets to implement
+// TODO: Check how to enable the user to select what text format should be used
+// TODO: Handle widget taps
+// TODO: Provide widget preview to system https://developer.android.com/develop/ui/views/appwidgets/enhance#updated-preview-with-glance
+// TODO: Update widget when data changes https://developer.android.com/develop/ui/compose/glance/glance-app-widget#update-glanceappwidget
+class MensinatorWidget(
+    private val periodPrediction: IPeriodPrediction,
+) : GlanceAppWidget() {
+
+    companion object {
+        private val SMALL_BOX = DpSize(90.dp, 90.dp)
+        private val BIG_BOX = DpSize(180.dp, 180.dp)
+        private val ROW = DpSize(180.dp, 48.dp)
+        private val LARGE_ROW = DpSize(300.dp, 48.dp)
+        private val COLUMN = DpSize(48.dp, 180.dp)
+        private val LARGE_COLUMN = DpSize(48.dp, 300.dp)
+    }
+
+    override val sizeMode = SizeMode.Responsive(
+        setOf(SMALL_BOX, BIG_BOX, ROW, LARGE_ROW, COLUMN, LARGE_COLUMN)
+    )
+
+
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
-            //val daysUntilNextPeriod = getNextPeriodDate(context)
-            val daysUntilNextPeriod = "7 days to next period"
+            val daysUntilNextPeriod = formatDaysUntilPeriod(
+                periodPrediction.getPredictedPeriodDate(),
+                format = NextPeriodFormat.OnlyDays
+            )
             WidgetUI(daysUntilNextPeriod)
         }
     }
 
-    @Composable
-    fun WidgetUI(daysUntilNextPeriod: String) {
-        Column(
-            modifier = GlanceModifier
-                .fillMaxSize()
-                .background(GlanceTheme.colors.background)
-                .padding(10.dp),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = daysUntilNextPeriod,
-                style = androidx.glance.text.TextStyle(
-                    color = GlanceTheme.colors.primary,
-                    fontSize = 18.sp
-                ),
-                modifier = GlanceModifier.clickable(
-                    actionStartActivity<MainActivity>()
-                )
-            )
+    private fun formatDaysUntilPeriod(
+        date: LocalDate?,
+        format: NextPeriodFormat
+    ): String {
+        val daysUntilNextPeriod = LocalDate.now().until(date).days
+        return when (format) {
+            NextPeriodFormat.MediumLengthText -> {
+                if (date == null) {
+                    "Unknown"
+                } else {
+                    "Period in $daysUntilNextPeriod days"
+                }
+            }
+            NextPeriodFormat.OnlyDays -> {
+                if (date == null) {
+                    "?"
+                } else {
+                    "$daysUntilNextPeriod"
+                }
+            }
         }
     }
 }
 
+@Composable
+@GlanceComposable
+fun WidgetUI(daysUntilNextPeriod: String) {
+    Column(
+        modifier = GlanceModifier
+            .fillMaxSize()
+            .background(GlanceTheme.colors.background)
+            .padding(10.dp)
+            .clickable(
+                actionStartActivity<MainActivity>()
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = daysUntilNextPeriod,
+            style = TextStyle(
+                color = GlanceTheme.colors.primary,
+                fontSize = 18.sp
+            ),
+        )
+    }
+}
+
 class MensinatorWidgetReceiver : GlanceAppWidgetReceiver() {
-    override val glanceAppWidget = MensinatorWidget()
+    private val koin = GlobalContext.get()
+    override val glanceAppWidget: MensinatorWidget = koin.get()
 
     override fun onEnabled(context: Context?) {
         super.onEnabled(context)
         context?.let {
             CoroutineScope(Dispatchers.IO).launch {
-                MensinatorWidget().updateAll(it)
+                glanceAppWidget.updateAll(it)
             }
         }
     }
