5d3906c46fdfaf9b23a679cfc1c641cd5369fc09
Carsten Hagemann <carsten@chagemann.de>
2025-05-04 22:28:04 +0200

    try out using a StateFlow to pass in PeriodDate, add todos

--- app/src/main/java/com/mensinator/app/business/PeriodPrediction.kt
+++ app/src/main/java/com/mensinator/app/business/PeriodPrediction.kt
@@ -1,11 +1,15 @@
 package com.mensinator.app.business
 
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import java.time.LocalDate
 
 class PeriodPrediction(
     private val dbHelper: IPeriodDatabaseHelper,
     private val calcHelper: ICalculationsHelper,
 ) : IPeriodPrediction {
+
+    private val _predictedPeriodDate = MutableStateFlow<LocalDate?>(null)
 
     override fun getPredictedPeriodDate(): LocalDate? {
         val periodCount = dbHelper.getPeriodCount()
@@ -15,4 +19,12 @@ class PeriodPrediction(
 
         return calcHelper.calculateNextPeriod()
     }
+
+    @Deprecated("HACK: Must be replaced by a service/repository that only updates the period prediction date consistently")
+    override fun updatePeriodPrediction() {
+        _predictedPeriodDate.tryEmit(getPredictedPeriodDate())
+    }
+
+    override val predictedPeriodDate: StateFlow<LocalDate?>
+        get() = _predictedPeriodDate
 }
--- app/src/main/java/com/mensinator/app/widgets/MensinatorWidget.kt
+++ app/src/main/java/com/mensinator/app/widgets/MensinatorWidget.kt
@@ -2,6 +2,8 @@ package com.mensinator.app.widgets
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -10,15 +12,18 @@ import androidx.glance.action.actionStartActivity
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.SizeMode
+import androidx.glance.appwidget.components.Scaffold
+import androidx.glance.appwidget.components.TitleBar
 import androidx.glance.appwidget.provideContent
-import androidx.glance.layout.Alignment
-import androidx.glance.layout.Column
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
 import androidx.glance.text.Text
+import androidx.glance.text.TextAlign
 import androidx.glance.text.TextStyle
 import com.mensinator.app.MainActivity
+import com.mensinator.app.R
 import com.mensinator.app.business.IPeriodPrediction
+import org.koin.compose.koinInject
 import java.time.LocalDate
 
 sealed interface NextPeriodFormat {
@@ -26,14 +31,15 @@ sealed interface NextPeriodFormat {
     data object MediumLengthText : NextPeriodFormat
 }
 
+// TODO: Data handling - Introduce repository/service to get updates?
+// related: start worker that runs on midnight to ensure "remaining days" figure is correct
+
 // TODO: Check what widgets to implement
 // TODO: Check how to enable the user to select what text format should be used
 // TODO: Handle widget taps
 // TODO: Provide widget preview to system https://developer.android.com/develop/ui/views/appwidgets/enhance#updated-preview-with-glance
 // TODO: Update widget when data changes https://developer.android.com/develop/ui/compose/glance/glance-app-widget#update-glanceappwidget
-class MensinatorWidget(
-    private val periodPrediction: IPeriodPrediction,
-) : GlanceAppWidget() {
+class MensinatorWidget() : GlanceAppWidget() {
 
     companion object {
         private val SMALL_BOX = DpSize(90.dp, 90.dp)
@@ -51,59 +57,67 @@ class MensinatorWidget(
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
-            val daysUntilNextPeriod = formatDaysUntilPeriod(
-                periodPrediction.getPredictedPeriodDate(),
-                format = NextPeriodFormat.OnlyDays
-            )
-            WidgetUI(daysUntilNextPeriod)
+            WidgetUI()
         }
     }
 
-    private fun formatDaysUntilPeriod(
-        date: LocalDate?,
-        format: NextPeriodFormat
-    ): String {
-        val daysUntilNextPeriod = LocalDate.now().until(date).days
-        return when (format) {
-            NextPeriodFormat.MediumLengthText -> {
-                if (date == null) {
-                    "Unknown"
-                } else {
-                    "Period in $daysUntilNextPeriod days"
-                }
-            }
-            NextPeriodFormat.OnlyDays -> {
-                if (date == null) {
-                    "?"
-                } else {
-                    "$daysUntilNextPeriod"
-                }
-            }
-        }
-    }
 }
 
 @Composable
 @GlanceComposable
-fun WidgetUI(daysUntilNextPeriod: String) {
-    Column(
-        modifier = GlanceModifier
-            .fillMaxSize()
-            .background(GlanceTheme.colors.background)
-            .padding(10.dp)
-            .clickable(
-                actionStartActivity<MainActivity>()
-            ),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalAlignment = Alignment.CenterVertically
+fun WidgetUI() {
+    val periodPrediction = koinInject<IPeriodPrediction>()
+    val predictedPeriodDate by periodPrediction.predictedPeriodDate.collectAsState()
+    val daysUntilNextPeriod = formatDaysUntilPeriod(
+        predictedPeriodDate,
+        format = NextPeriodFormat.OnlyDays
+    )
+
+    Scaffold(
+        backgroundColor = GlanceTheme.colors.widgetBackground,
+        titleBar = {
+            TitleBar(
+                startIcon = ImageProvider(R.drawable.baseline_bloodtype_24),
+                title = LocalContext.current.getString(R.string.app_name),
+            )
+
+        }
     ) {
         Text(
             text = daysUntilNextPeriod,
+            modifier = GlanceModifier
+                .padding(10.dp)
+                .clickable(
+                    actionStartActivity<MainActivity>()
+                ).fillMaxSize(),
             style = TextStyle(
                 color = GlanceTheme.colors.onSurface,
-                fontSize = 18.sp
+                fontSize = 18.sp,
+                textAlign = TextAlign.Center
             ),
         )
     }
 }
 
+private fun formatDaysUntilPeriod(
+    date: LocalDate?,
+    format: NextPeriodFormat
+): String {
+    val daysUntilNextPeriod = LocalDate.now().until(date).days
+    return when (format) {
+        NextPeriodFormat.MediumLengthText -> {
+            if (date == null) {
+                "Unknown"
+            } else {
+                "Period in $daysUntilNextPeriod days"
+            }
+        }
+        NextPeriodFormat.OnlyDays -> {
+            if (date == null) {
+                "?"
+            } else {
+                "$daysUntilNextPeriod"
+            }
+        }
+    }
+}
