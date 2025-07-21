ef5a6864f10682167323bbb5d4ddaded47bcd648
Carsten Hagemann <carsten@chagemann.de>
2025-07-21 19:52:03 +0200

    Add widget previews, improve data handling, update widgets on midnight

---
+++ app/src/main/java/com/mensinator/app/widgets/WidgetData.kt
@@ -0,0 +1,15 @@
+package com.mensinator.app.widgets
+
+import java.time.LocalDate
+
+data class WidgetData(
+    val daysUntilPeriodWithoutText: String,
+    val daysUntilPeriodWithText: String,
+    /*
+    val daysUntilOvulationWithoutText: String,
+    val daysUntilOvulationWithText: String,
+    val cycleDay: Int?,
+
+     */
+    val nextPeriod: LocalDate?
+)
