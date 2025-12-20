dbd6104be29879ab7af28888ab381807091a1a46
Emma <emma.tellblom@gmail.com>
2025-12-20 15:54:55 +0100

    upgraded some libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,9 +37,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
+
     buildFeatures {
         compose = true
     }
